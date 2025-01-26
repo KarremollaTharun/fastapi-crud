f20fd2dec14596d0dadc9c96d8ecf2a745b0971b
Karremolla Tharun <119946982+KarremollaTharun@users.noreply.github.com>
2025-01-26 23:15:13 +0530

    database.py

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1761@localhost/fastapi"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@localhost/database"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
